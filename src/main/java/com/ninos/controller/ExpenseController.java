3a2ef6b65a253f574d6ab22c60877296256252b6
NinoosMoshi <nalkaton@gmail.com>
2022-07-25 09:19:47 -0400

    set spring profiles

--- src/main/java/com/ninos/controller/ExpenseController.java
+++ src/main/java/com/ninos/controller/ExpenseController.java
@@ -1,6 +1,5 @@
 package com.ninos.controller;
 
-import com.ninos.dao.ExpenseDTO;
 import com.ninos.service.ExpenseService;
 
 import org.springframework.stereotype.Controller;
@@ -8,10 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import lombok.RequiredArgsConstructor;
 
-import java.math.BigDecimal;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
